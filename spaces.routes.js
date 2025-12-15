b465a5779bcfbf1cba3d5de4c236c81384922c8c
mjssantosx <jejejessicamaria@gmail.com>
2025-12-15 16:30:41 -0300

    Organizando o código!

--- spaces.routes.js
+++ spaces.routes.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const router = express.Router();
 const {
-  getSpaces,
-  getSpaceById
+getSpaces,
+getSpaceById
 } = require("../controllers/spaces.controller");
 
 router.get("/", getSpaces);
 
 router.get("/:id", getSpaceById);
+
 
 module.exports = router;
