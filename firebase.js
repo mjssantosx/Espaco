5120e362e1fd7c7394ad2c76c80a91b1c28e4b7f
mjssantosx <jejejessicamaria@gmail.com>
2025-12-15 22:02:14 -0300

    outra remoção de erro no codigo

--- firebase.js
+++ firebase.js
@@ -1,11 +1,11 @@
 const admin = require("firebase-admin");
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
   databaseURL: "https://projeto-35f39-default-rtdb.firebaseio.com/"
 });
 
 const db = admin.database();
 
 module.exports = db;
+
 
